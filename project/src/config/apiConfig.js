7aa0a4b9c5d66a460b87ef8e3cf2ed5a4bc3700a
Dnyaneshwar Balaji Kalwale <prembkalwale@gmail.com>
2025-01-19 02:18:00 +0530

    Traffic relief project

--- project/src/config/apiConfig.js
+++ project/src/config/apiConfig.js
@@ -2,8 +2,10 @@ export const API_KEYS = {
   GOOGLE_MAPS: 'AIzaSyDjru22GVT4Uq0Er_QHnhBVJ20gWqyUo7Q',
   WEATHER: '69e56a1473fd616d1e030b9a33a33fbb',
   SPOTIFY_ENDPOINT: 'https://api.spotify.com/v1',
-  // TWILIO: {
-  //   SID: 'SK883b9fcbe6eb85053b64b63ba05688b8',
-  //   SECRET: '4qeYX7BNK41mIxXqAl70wIehGbSNGXgz'
-  // }
+  TWILIO: {
+  SID: 'SK883b9fcbe6eb85053b64b63ba05688b8',
+  SECRET: '4qeYX7BNK41mIxXqAl70wIehGbSNGXgz'
+}
+
 };
+
